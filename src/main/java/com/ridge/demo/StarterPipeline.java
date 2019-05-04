b1f32e0b884b79d1ed0933427bd1d8d936632bd4
John Clarke <themagoo@hotmail.co.uk>
2019-05-04 20:39:40 +0100

    Initial commit#

---
+++ src/main/java/com/ridge/demo/StarterPipeline.java
@@ -0,0 +1,94 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ridge.demo;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SimpleFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
+
+/**
+ * A starter example for writing Beam programs.
+ *
+ * <p>The example takes two strings, converts them to their upper-case
+ * representation and logs them.
+ *
+ * <p>To run this starter example locally using DirectRunner, just
+ * execute it without any additional parameters from your favorite development
+ * environment.
+ *
+ * <p>To run this starter example using managed resource in Google Cloud
+ * Platform, you should specify the following command-line options:
+ *   --project=<YOUR_PROJECT_ID>
+ *   --stagingLocation=<STAGING_LOCATION_IN_CLOUD_STORAGE>
+ *   --runner=DataflowRunner
+ */
+public class StarterPipeline {
+  private static final Logger LOG = LoggerFactory.getLogger(StarterPipeline.class);
+
+  public static void main(String[] args) {
+    Pipeline p = Pipeline.create(
+        PipelineOptionsFactory.fromArgs(args).withValidation().create());
+
+    p.apply("read from PubSub",
+      PubsubIO.readStrings().fromSubscription("projects/gfs-ecm/subscriptions/df-wibble-test-sub")
+    )
+    .apply("JSON Parse message and Log events",
+    ParDo.of(
+      new DoFn<String, Void>() {
+        @ProcessElement
+        public void ParseAndFilterElement(ProcessContext c) {
+          JSONParser jsonParser = new JSONParser();
+          JSONObject jsonMessage = null;
+
+          try {
+            // Parse the context as a JSON object:
+            jsonMessage = (JSONObject) jsonParser.parse(c.element());
+            if(((String)jsonMessage.get("type")).equals("event")) {
+              LOG.info("Found an event!");
+            }
+          } catch (Exception e) {
+            LOG.warn(String.format("Exceptuin encountered parsing JSON (%s) ...", e));
+          }
+        }
+      }
+    ));
+/*
+    .apply("Log out messages",
+    ParDo.of(
+      new DoFn<String, Void>() {
+        @ProcessElement
+        public void processElement(ProcessContext c)  {
+          LOG.info(c.element());
+      }
+    }));
+*/
+    p.run();
+  }
+}
